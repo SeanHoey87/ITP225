9f376ad60588de6256444f3ea8526aeb92fd2a8d
SeanHoey87 <seanhoey87@gmail.com>
2025-01-23 15:49:29 -0500

    Create HTML+PHP.php

---
+++ HTML+PHP.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1 HTML + PHP</title>
+</head>
+<body>
+    <h1>Sean Hoey</h1>
+    <p>
+        <?php
+            echo "My experiance with programming is slightly above beginner level. I can code simple scripts at a decent pace, primarily in Java, and more moderate level applications and scripts
+            might take longer depending on difficulty. Currently I'm working on improving my skills by working leetcode problems and learning Algorithms and Data Structures. '
+            I enjoy working on projects that I would find useful for example on the side ive been working on a Discord bot learning about JDA (Java Discord API).".'<br>';
+
+            echo '<br>'."<a href=\"https://www.bls.gov/ooh/computer-and-information-technology/computer-programmers.htm#tab-1\" target=\"_blank\" >Link to the bureau of labor statistics</a>";
+        ?>
+    </p>
+
+</body>
+</html>
