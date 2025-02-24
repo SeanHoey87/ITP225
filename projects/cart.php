07ca8885f7c099a6c761c15284630103852d5f98
SeanHoey87 <seanhoey87@gmail.com>
2025-02-23 20:52:46 -0500

    small changes to looks

--- projects/cart.php
+++ projects/cart.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Cart</title>
-    <link rel="stylesheet" href=".../css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <header>
         <h1>Your Shopping Cart</h1>
-        <a href="index.php">🏠 Continue Shopping</a>
+        <a href="index.php">🛍️ Continue Shopping</a>
     </header>
 
     <table>
